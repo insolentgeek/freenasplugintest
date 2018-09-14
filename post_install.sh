39f142e2a7aebd6e8e70475edf0eead5a5944340
jaxjexjox <jaxjexjox@users.noreply.github.com>
2018-09-14 11:18:20 +1000

    Update post_install.sh
    Removed several un-needed commands and fixed the notes, this should work fine now.

--- post_install.sh
+++ post_install.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 #
-# Post install script for first test plugin.
-# Written by jaxjexjox (Disk Diddler) @FreeNAS
-# (I have manually performed all of these commands in a fresh, manually created jail and ended up with a working version of SabNZBD (sabnzbdplus)
-# Hence the attempt to try and learn to make a plugin and get this script to do it for me
-#
-# pkg install sabnzbdplus
-# I'm fairly certain, this is actually not required, as it's a post install script, but I originally had this line in here.
+# Post install script for installing SabNZBDPlus plugin
+# Written by jaxjexjox (Disk Diddler) @FreeNAS 
+# This has been tested working 14/Sept/2018
+# Please note, very much an amateur plugin writer
 #
 service sabnzbd onestart
 sleep 5
 service sabnzbd onestop
-#
-# Start and stop the plugin, Sab  create some folders and files you need to change
-#
 sleep 5
 #
-cp /usr/local/sabnzbdprep.ini /usr/local/sabnzbd/sabnzbd.ini
+# Start and stop the plugin, Sab will create some folders and files which need some reconfiguring.
+#
+#
+mv /usr/local/sabnzbdprep.ini /usr/local/sabnzbd/sabnzbd.ini
 chown _sabnzbd:_sabnzbd /usr/local/sabnzbd/sabnzbd.ini
 #
 # This replaces the sabnzbd.ini file with a corrected file and sets permissions
 # Corrected version changes the "host" string from 127.0.0.1 to 0.0.0.0
-# I am unsure why, but until changed, the WebUI will not work. 
+# I am unsure why, but until changed, the WebUI will not work
 #
 #
 #
